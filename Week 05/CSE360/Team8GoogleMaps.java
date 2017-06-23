e8fe719e05507d5dc67474538ba6c539a0235e39
slapski <msslapskio@gmail.com>
2017-06-22 20:29:22 -0700

    changed panel size

--- Week 05/CSE360/Team8GoogleMaps.java
+++ Week 05/CSE360/Team8GoogleMaps.java
@@ -3,36 +3,36 @@ package CSE360;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
-
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
 public class Team8GoogleMaps {
-	private String api_url = "https://maps.googleapis.com/maps/api/staticmap?";
-	private String api_key = "AIzaSyCPBkB4FfvXzIYRgbRBmAMxqXLhnaGJVXU";
-	private String zoom = "zoom=11&";
-	private String size = "size=612x612&";
-	private String scale = "scale=1&";
-	private String mapType = "maptype=roadmap&";
-	ImageIcon img;
 
-	Team8GoogleMaps(double latitude, double longitude) {
+    private ImageIcon img;
 
-		String center = "center=" + latitude +"," + longitude + "&";
+    public Team8GoogleMaps(double latitude, double longitude, int width, int height) {
 
-		api_url = api_url + center + zoom + size + scale + mapType + "key=" + api_key;
+        String api_url = "https://maps.googleapis.com/maps/api/staticmap?";
+        String api_key = "AIzaSyCPBkB4FfvXzIYRgbRBmAMxqXLhnaGJVXU";
+        String zoom = "zoom=9&";
+        String size = "size=" + width + "x" + height + "&";
+        String scale = "scale=1&";
+        String mapType = "maptype=roadmap&";
 
-		try {
-			URL url = new URL(api_url);
-			BufferedImage bufimg = ImageIO.read(url);
-			img = new ImageIcon(bufimg);
-		} catch (IOException e) {
-			System.exit(1);
-		}
-	}
+        String center = "center=" + latitude + "," + longitude + "&";
 
-	public ImageIcon getImage() {
-		return img;
-	}
+        api_url = api_url + center + zoom + size + scale + mapType + "key=" + api_key;
 
+        try {
+            URL url = new URL(api_url);
+            BufferedImage bufferedImage = ImageIO.read(url);
+            img = new ImageIcon(bufferedImage);
+        } catch (IOException e) {
+            System.exit(1);
+        }
+    }
+
+    public ImageIcon getImage() {
+        return img;
+    }
 }
