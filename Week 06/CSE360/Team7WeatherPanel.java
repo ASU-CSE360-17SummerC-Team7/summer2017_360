d42ea1eb824448f75ab9972680ebebb43a22190c
pdreiter <cse360summerteam7@gmail.com>
2017-06-22 18:14:14 -0700

    Fixing Team7WeatherPanel for layout

--- Week 06/CSE360/Team7WeatherPanel.java
+++ Week 06/CSE360/Team7WeatherPanel.java
@@ -37,94 +37,30 @@ import java.awt.font.*;
 public class Team7WeatherPanel extends JPanel
 {
     private Team7WeatherInfo geoLocation;
-    private ImageIcon weatherIcon;
-    private JLabel weatherLabel;
-    // Variables declaration - do not modify                     
-    private javax.swing.JPanel IconImage;
-    private javax.swing.JTextArea TextSummary;
-    private javax.swing.JTextArea TextCloudCover;
-    private javax.swing.JTextArea TextPrecipitationProbability;
-    private javax.swing.JTextArea TextTemperature;
-    private javax.swing.JTextArea TextHumidity;
-    private static String iconPath;
+    private JLabel temp;
     private int xbound;
     private int ybound;
     // End of variables declaration 
     /**
      * Creates new form WeatherPanel
      */
-    public Team7WeatherPanel(double latitude, double longitude,String iP, int xb, int yb)  {
+    public Team7WeatherPanel(double latitude, double longitude,int xb, int yb)  {
+        setLayout(new BorderLayout());
         xbound=xb;ybound=yb;
         geoLocation = new Team7WeatherInfo(latitude,longitude);
-        iconPath=iP;
-        initComponents();
-        setOpaque(false);
+        temp = new JLabel(geoLocation.getWeatherFieldString("currently", "temperature")+"\u00b0"+"F");//,SwingConstants.LEFT);
+        System.out.println("Initial temperature: "+geoLocation.getWeatherFieldString("currently", "temperature")+"\u00b0"+"F");
+        temp.setHorizontalAlignment(JLabel.LEFT);
+        add(temp,BorderLayout.SOUTH);
+        setOpaque(true);
+        setVisible(true);
+        setSize(xb,yb);
     }
 
-    @SuppressWarnings("unchecked")
-    private void initComponents()  {
-        setBackground(Color.white);
-        Font uniformFont = new Font("Courier",Font.TRUETYPE_FONT,8);
-        TextSummary = new javax.swing.JTextArea(2,4);
-        TextSummary.setFont(uniformFont);
-        TextHumidity = new javax.swing.JTextArea(2,10);
-        TextHumidity.setFont(uniformFont);
-        TextTemperature = new javax.swing.JTextArea(2,10);
-        TextTemperature.setFont(uniformFont);
-        TextPrecipitationProbability = new javax.swing.JTextArea(2,10);
-        TextPrecipitationProbability.setFont(uniformFont);
-        TextCloudCover = new javax.swing.JTextArea(2,10);
-        TextCloudCover.setFont(uniformFont);
-        weatherIcon = new ImageIcon((new ImageIcon(iconPath+"/"+geoLocation.getWeatherFieldString("currently","icon")+".png").getImage().getScaledInstance(xbound, xbound,
-                java.awt.Image.SCALE_SMOOTH)),"Weather");
-        
-        weatherLabel = new JLabel("", weatherIcon, JLabel.CENTER);
-        weatherLabel.setIcon(weatherIcon);
-        
-        setLayout(new java.awt.GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.BOTH;
-        c.gridx=0;c.gridy=0;
-        c.gridheight=1;
-        c.gridwidth=1;
-        c.weighty = 0.333;
-        c.weightx = 1;
-        weatherLabel.setOpaque(true);
-        weatherLabel.setBackground(Color.white);
-        add(weatherLabel,c);
-        c.gridx=0;c.gridy=1;
-        TextSummary.setText(geoLocation.getWeatherFieldString("currently", "summary"));
-        
-        add(TextSummary,c);
-        TextHumidity.setText(geoLocation.getWeatherFieldString("currently", "humidity")+"[Humidity]");
-        c.gridx=0;c.gridy=2;
-        add(TextHumidity,c);
-
-        TextTemperature.setText(geoLocation.getWeatherFieldString("currently", "temperature")+"\u00b0"+" F[Temp]");
-        c.gridx=0;c.gridy=3;
-        add(TextTemperature,c);
-
-        TextPrecipitationProbability.setText(geoLocation.getWeatherFieldString("currently", "precipProbability")+"[PrecipProb]");
-        c.gridx=0;c.gridy=4;
-        add(TextPrecipitationProbability,c);
-
-        TextCloudCover.setText(geoLocation.getWeatherFieldString("currently", "cloudCover")+"[CloudCover]");
-        c.gridx=0;c.gridy=5;
-        add(TextCloudCover,c);
-    }
-
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {
-        // TODO add your handling code here:
-    }
     public void updateWeatherPanel(double latitude,double longitude) { 
         geoLocation.UpdateGeoLocation(latitude, longitude);
-        weatherLabel.setIcon(new ImageIcon((new ImageIcon(iconPath+"/"+geoLocation.getWeatherFieldString("currently","icon")+".png").getImage().getScaledInstance(xbound, xbound,
-                java.awt.Image.SCALE_SMOOTH)),"Weather"));
-        TextSummary.setText(geoLocation.getWeatherFieldString("currently", "summary"));
-        TextHumidity.setText(geoLocation.getWeatherFieldString("currently", "humidity")+"\n[Humidity]");
-        TextTemperature.setText(geoLocation.getWeatherFieldString("currently", "temperature")+"\u00b0"+" F[Temp]");
-        TextPrecipitationProbability.setText(geoLocation.getWeatherFieldString("currently", "precipProbability")+"[PrecipProb]");
-        TextCloudCover.setText(geoLocation.getWeatherFieldString("currently", "cloudCover")+"[CloudCover]");
+        temp.setText(geoLocation.getWeatherFieldString("currently", "temperature")+"\u00b0"+"F");
+        System.out.println("Updated temperature: "+geoLocation.getWeatherFieldString("currently", "temperature")+"\u00b0"+"F");
     }
 }
 
